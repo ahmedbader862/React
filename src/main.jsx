ed290408be9d88740f8cb71b06a712fefc1f87ce
Ahmed badr <abobader862@gmail.com>
2025-02-03 13:45:21 +0200

    donee

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
 import "react-image-gallery/styles/css/image-gallery.css";
 
-=======
 import { Provider } from 'react-redux';
 import { mystore } from './Redux/store.jsx';
->>>>>>> redux
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
